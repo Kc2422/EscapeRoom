8741576b0f15aaabf528ba8831b89e3908fee0a6
tbui22 <tbui22@gmail.com>
2022-05-21 09:21:58 -0400

    Basic Views and Routes set up.

--- client/src/views/PaintingView.js
+++ client/src/views/PaintingView.js
@@ -2,28 +2,30 @@ import React, {useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import './Door.css';
 
-const FrontDoorView = (props) => {
+const BookshelfView = (props) => {
     
     const history = useHistory()
     
 
     const onClickRightHandler = (e) => {
-        history.push("/bookshelf")
+        history.push("/lights")
     }
 
     const onClickLeftHandler = (e) => {
-        history.push("/lights")
+        history.push("/bookshelf")
     }
 
     return(
         <div className="gameWindow position-relative">  
-            <img className="gameBackground" src={require(props.solvedPuzzle1 ? '../img/DoorWithOpenCabinet.png' : '../img/DoorWithClosedCabinet.png')} alt="Front Door"/>
+            <img className="gameBackground" src={require('../img/PaintingsWall.png')} alt="Paintings Wall"/>
             <img className="position-absolute top-50 start-0 translate-middle-y" src={require('../img/leftArrow.png')} alt='left arrow' onClick={onClickLeftHandler} />
             <img className="position-absolute top-50 end-0 translate-middle-y" src={require('../img/rightArrow.png')} alt='right arrow' onClick={onClickRightHandler}/>
             <img className="rug position-absolute bottom-0 start-50 translate-middle-x" src={require('../img/rug.png')} alt="rug" />
-            <img className="lock" src={require("../img/lock.png")} alt="lock" />
+            
+            {/* NEED a MAP of ARRAY of Paintings. Change index as we click and drop */}
+
         </div>
     )
 }
 
-export default FrontDoorView;
+export default BookshelfView;
