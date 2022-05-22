a5f4c497a157f5f9e49b1918958cf798d63ddcf8
tbui22 <tbui22@gmail.com>
2022-05-21 22:43:48 -0400

    all the images are clickable

--- client/src/views/LightsView.js
+++ client/src/views/LightsView.js
@@ -15,12 +15,29 @@ const LightsView = (props) => {
         history.push("/paintings")
     }
 
+    const onClickLightSwitch = (e) => {
+        console.log("you clicked switch!");
+    }
+
+    const onClickLightSafe = (e) => {
+        console.log("you clicked safe!");
+    }
+
+
+
     return(
         <div className="gameWindow position-relative">  
-            <img className="gameBackground" src={require('../img/Wall.jpg')} alt="Lights Wall"/>
-            <img className="position-absolute top-50 start-0 translate-middle-y clickable" src={require('../img/leftArrow.png')} alt='left arrow' onClick={onClickLeftHandler} />
-            <img className="position-absolute top-50 end-0 translate-middle-y clickable" src={require('../img/rightArrow.png')} alt='right arrow' onClick={onClickRightHandler}/>
+            <img className="gameBackground" src={require('../img/Wall.jpg')} alt="Lights Wall" />
+
+            <img className="arrow position-absolute top-50 start-0 translate-middle-y clickable" src={require('../img/leftArrow.png')} alt='left arrow' onClick={onClickLeftHandler} />
+            <img className="arrow position-absolute top-50 end-0 translate-middle-y clickable" src={require('../img/rightArrow.png')} alt='right arrow' onClick={onClickRightHandler}/>
+            <img className="lights position-absolute top-0 end-0 translate-middle-y clickable" src={require('../img/lightSwitches.png')} alt='Light Switches' onClick={onClickLightSwitch} />
+            <img className="safe position-absolute top-50 end-0 translate-middle-y clickable" src={require('../img/safe1.png')} alt='Safe' onClick={onClickLightSafe} />
             <img className="rug position-absolute bottom-0 start-50 translate-middle-x" src={require('../img/rug.png')} alt="rug" />
+            <img className="drawer position-absolute top-50 start-50 translate-middle clickable" src={require('../img/drawer.png')} alt="drawer" />
+            
+            {/* <img className="table position-absolute top-50 start-50 translate-middle" src={require('../img/table.jpg')} alt="table" /> */} 
+            {/* for some reason can't set the width for the table */}
             
         </div>
     )
