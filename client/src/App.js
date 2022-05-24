78f32ae4ee5dad498873ce4400a907ab29c40efc
tbui22 <tbui22@gmail.com>
2022-05-24 19:41:57 -0400

    music

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import ReactAudioPlayer from 'react-audio-player';
 import './App.css';
 import Home from './views/Home';
 import React, { useEffect, useState } from 'react'
@@ -23,6 +24,8 @@ import Success from './views/Success';
 import BestTimes from './views/BestTimes';
 import TestMap from './views/TestMap';
 
+import Triller from './files/Triller.m4a';
+import { Howl, Howler } from 'howler';
 
 
 const Beach = require("./img/ArrFrameBeach.png")
@@ -34,6 +37,8 @@ const Mountain = require("./img/ArrFrameMountain.png")
 
 
 function App() {
+  const { Howl, Howler } = require('howler');
+  Howler.volume(0.05);
   const [name, setName] = useState(localStorage.getItem('name'))
   const [submitted, setSubmitted] = useState(localStorage.getItem('submitted'))
   const [text, setMessages] = useState(["Initial Story", "you are locked in the cabin try to get out"])
@@ -54,6 +59,8 @@ function App() {
 
   const [cabinetKeyVisible, setCabinetKeyVisible] = useState(true);
   const [finalKeyVisible, setFinalKeyVisible] = useState(true);
+
+  const [audio, setAudio] = useState(true);
 
 
   const grabFinalKey = () => {
@@ -94,9 +101,12 @@ function App() {
   const onClickKeyHandler = (e) => {
     addMessage("You found a key! You added it to your inventory.");
     setCabinetKeyVisible(false);
-}
+  }
 
-
+  const music = new Howl({
+    src: [Triller],
+    autoplay: true,
+  });
 
   useEffect(() => {
     localStorage.setItem('sidebar', sidebar)
@@ -136,52 +146,63 @@ function App() {
 
   }, [submitted, counter])
 
+  const audioIcon = './img/sound.png';
+  const onClickAudio = (e) => {
+    setAudio(!audio);
+    if (audio === true) {
+      audioIcon = './img/sound/png';
+    } else {
+      audioIcon = './img/mute.png';
+    }
+  }
+
   return (
     <>
 
       <Navbar name={name} submitted={submitted} />
 
-      <Sidebar sidebar={sidebar} text={text} />
-    <div className='needBackground'>
+      <Sidebar sidebar={sidebar} text={text} audioIcon={audioIcon} onClickAudio={onClickAudio}/>
+      <div className='needBackground'>
 
-      <Route exact path="/">
-        <Home changeName={changeName} name={name} setSubmitted={changeSubmitted} showSidebar={showSidebar} />
-      </Route>
+        <Route exact path="/">
+          <Home changeName={changeName} name={name} setSubmitted={changeSubmitted} showSidebar={showSidebar} />
+        </Route>
 
-      <Route exact path="/door">
-        <FrontDoorView onClickKeyHandler={onClickKeyHandler} cabinetKeyVisible={cabinetKeyVisible} addMessage={addMessage} hasKey={hasKey} solvedLights={solvedLights} name={name} minute={minute} second={second} />
-      </Route>
+        <Route exact path="/door">
+          <FrontDoorView onClickKeyHandler={onClickKeyHandler} cabinetKeyVisible={cabinetKeyVisible} addMessage={addMessage} hasKey={hasKey} solvedLights={solvedLights} name={name} minute={minute} second={second} />
+        </Route>
 
-      <Route exact path="/bookshelf">
-        <BookshelfView addMessage={addMessage} SolvedPuzzleLights={SolvedPuzzleLights} text={text} solvedLights={solvedLights} sidebar={sidebar} name={name} submitted={submitted} />
-      </Route>
+        <Route exact path="/bookshelf">
+          <BookshelfView addMessage={addMessage} SolvedPuzzleLights={SolvedPuzzleLights} text={text} solvedLights={solvedLights} sidebar={sidebar} name={name} submitted={submitted} />
+        </Route>
 
-      <Route exact path="/paintings">
-        <PaintingView pictures={pictures} solvedLights={solvedLights} correctOrder={correctOrder}
-          changePics={changePics} setInOrder={isInOrder}
-          addMessage={addMessage} />
-      </Route>
+        <Route exact path="/paintings">
+          <PaintingView pictures={pictures} solvedLights={solvedLights} correctOrder={correctOrder}
+            changePics={changePics} setInOrder={isInOrder}
+            addMessage={addMessage} />
+        </Route>
 
-      <Route exact path="/lights">
-        <LightsView finalKeyVisible={finalKeyVisible} grabFinalKey={grabFinalKey} cabinetKeyVisible={cabinetKeyVisible} inOrder={inOrder} addMessage={addMessage} SolvedPuzzleLights={SolvedPuzzleLights} text={text} solvedLights={solvedLights} sidebar={sidebar} name={name} submitted={submitted} />
-      </Route>
+        <Route exact path="/lights">
+          <LightsView finalKeyVisible={finalKeyVisible} grabFinalKey={grabFinalKey} cabinetKeyVisible={cabinetKeyVisible} inOrder={inOrder} addMessage={addMessage} SolvedPuzzleLights={SolvedPuzzleLights} text={text} solvedLights={solvedLights} sidebar={sidebar} name={name} submitted={submitted} />
+        </Route>
 
 
-      <Route exact path="/success">
-        <Success haskey={hasKey} name={name} second={second} minute={minute} changeSubmitted={changeSubmitted} showSidebar={showSidebar} />
-      </Route>
-      <Route exact path="/cheater">
+        <Route exact path="/success">
+          <Success haskey={hasKey} name={name} second={second} minute={minute} changeSubmitted={changeSubmitted} showSidebar={showSidebar} />
+        </Route>
+        <Route exact path="/cheater">
 
-      </Route>
+        </Route>
 
-      <Route exact path = "/times">
-        <BestTimes/>
-      </Route>
+        <Route exact path="/times">
+          <BestTimes />
+        </Route>
 
-      <Route exact path = "/test">
-        <TestMap/>
-      </Route>
-    </div>
+        <Route exact path="/test">
+          <TestMap />
+        </Route>
+
+      </div>
 
     </>
   );
